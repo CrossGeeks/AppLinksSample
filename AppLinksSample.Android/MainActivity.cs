fe104a538e8ff816ff2e7500b28cb3a1bb2044cd
Rendy Del Rosario <rendy.delrosario@gmail.com>
2019-01-07 23:27:59 -0400

    Adding sample

---
+++ AppLinksSample.Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace AppLinksSample.Droid
+{
+    [Activity(Label = "AppLinksSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme",
+              MainLauncher = true,  
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    //Invite App Link
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "https",
+                  DataHost = "xamboy.com",
+                  DataPathPrefix = "/hello",
+                  AutoVerify =true,
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+                  DataScheme = "http",
+                  DataHost = "xamboy.com",
+                  AutoVerify = true,
+                  DataPathPrefix = "/hello",
+                  Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+        }
+    }
+
+    
+}
+
---
+++ AppLinksSample/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppLinksSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
+        {
+            if (uri.Host.EndsWith("xamboy.com", StringComparison.OrdinalIgnoreCase))
+            {
+
+                if (uri.Segments != null && uri.Segments.Length == 3)
+                {
+                    var action = uri.Segments[1].Replace("/", "");
+                    var msg = uri.Segments[2];
+
+                    switch (action)
+                    {
+                        case "hello":
+                            if(!string.IsNullOrEmpty(msg)){
+                                Device.BeginInvokeOnMainThread(async() =>
+                                {
+                                    await Current.MainPage.DisplayAlert("hello", msg.Replace("&", " "), "ok");
+                                });
+                            }
+                              
+                            break;
+
+                        default:
+                            Xamarin.Forms.Device.OpenUri(uri);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
